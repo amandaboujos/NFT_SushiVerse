8a4c8065b49161b0a7a77ae7ba78b93f6b318d4b
amandaboujos <amandaboujos@hotmail.com>
2022-04-21 14:03:22 -0400

    scripts

--- Assets/PortalScripts/BechToArcade.cs
+++ Assets/PortalScripts/BechToArcade.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BechToArcade : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    void OnTriggerEnter(Collider gameObjectInformation)
+    {
+        if (gameObjectInformation.gameObject.tag == "Player")
+        {
+            Debug.Log("Collision Detected");
+            SceneManager.LoadScene("Arcade");
+        }
+
+        //Debug.Log(gameObjectInformation.gameObject.name); 
+
+
+    }
+
 }
+
